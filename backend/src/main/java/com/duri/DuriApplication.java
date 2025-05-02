a4d964a5f7764d61b15a645d6e944fcd6f2a5b2d
YongU Cho <yongu2000@naver.com>
2025-05-02 21:32:42 +0900

    project setup

---
+++ backend/src/main/java/com/duri/DuriApplication.java
@@ -0,0 +1,13 @@
+package com.duri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuriApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/duri/DuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
