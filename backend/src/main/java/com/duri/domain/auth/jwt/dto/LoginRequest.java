1e25c7ce3f04a159a9511a51846fdcdf5b5ae2c2
Yong U Cho <yongu2000@naver.com>
2025-06-06 04:00:46 +0900

    test: JWT 필터 단위테스트 작성 JwtAuthenticationFilter, JwtLoginFilter

--- backend/src/main/java/com/duri/domain/auth/jwt/dto/LoginRequest.java
+++ backend/src/main/java/com/duri/domain/auth/jwt/dto/LoginRequest.java
@@ -1,14 +1,16 @@
 package com.duri.domain.auth.jwt.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
 
     private String emailOrUsername;
     private String password;
     private boolean rememberMe;
-    
+
 }
