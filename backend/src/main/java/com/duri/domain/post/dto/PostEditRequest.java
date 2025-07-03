9a659a448c71524c3bbc68dd3f5b6ef6a11f0302
Yong U Cho <yongu2000@naver.com>
2025-07-03 16:48:53 +0900

    fix: PostEditRequest @NoArgsConstructor 추가 직렬화

--- backend/src/main/java/com/duri/domain/post/dto/PostEditRequest.java
+++ backend/src/main/java/com/duri/domain/post/dto/PostEditRequest.java
@@ -6,8 +6,10 @@ import java.time.LocalDate;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class PostEditRequest {
 
